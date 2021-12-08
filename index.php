5287437d736f34575e6a5bced97ef27cb6f800ba
Thanura Navodya <59390864+ThanuraN3@users.noreply.github.com>
2021-12-09 02:24:32 +0530

    index

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <header>
         <a href="#" class="brand">CEYLON</a>
         <div class="navigation">
-            <div class="navigation-item">
+            <div class="navigation-items">
                 <a href="#">Home</a>
                 <a href="#">Gallery</a>
                 <a href="#">Explore</a>
@@ -19,6 +19,19 @@
             </div>
         </div>
     </header>
+<section class="home">
+    <div class="content">
+        <h1>Beautiful<br><span>Beaches</span></h1>
+        <p>Sri Lanka is a beautiful island surrounded by oceans. It has some of the most beautiful beaches and is a major tourist attraction. A large number of foreigners come to Sri Lanka every year to surf, dive, see coral reefs and watch bluewales.</p>
+        <a href="3">Read More</a>
+    </div>
+    <div class="media-icons">
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-Youtube"></i></a>
+    </div>
+    
+</section>
 
 </body>
 </html>
