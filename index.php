7dc51ffbeb3242e3b858f97f7247817216c31892
Thanura Navodya <59390864+ThanuraN3@users.noreply.github.com>
2021-12-09 00:54:47 +0530

    index

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<?php
+<html lang="en" dir="ltr">
+<head>
+    <meta charest="utf-8">
+    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+    <title>Ceylon - Sri lankan travel guide website</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+</head>
+<body>
+    <header>
+        <a href="#" class="brand">CEYLON</a>
+        <div class="navigation">
+            <div class="navigation-item">
+                <a href="#">Home</a>
+                <a href="#">Gallery</a>
+                <a href="#">Explore</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </div>
+        </div>
+    </header>
+
+</body>
+</html>
+
